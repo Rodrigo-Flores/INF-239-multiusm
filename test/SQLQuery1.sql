c128f591b0919150dc6fa3e5b77e9d80b286e7d9
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-09-30 03:44:52 -0300

    [UDT] test folder

---
+++ test/SQLQuery1.sql
@@ -0,0 +1,8 @@
+select * from test_tarea1_inf239.dbo.Oferta;
+select * from test_tarea1_inf239.dbo.Boleta;
+select * from test_tarea1_inf239.dbo.Carrito;
+select * from test_tarea1_inf239.dbo.productos;
+
+delete from test_tarea1_inf239.dbo.Boleta;
+delete from test_tarea1_inf239.dbo.Carrito;
+delete from test_tarea1_inf239.dbo.Oferta;
