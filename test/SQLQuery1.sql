c31152d33a847d875c3d95024361fd6cea585a9d
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-10-07 23:07:58 -0300

    [ADD] frop tables

--- test/SQLQuery1.sql
+++ test/SQLQuery1.sql
@@ -1,8 +1,20 @@
+delete from test_tarea1_inf239.dbo.Boleta;
+delete from test_tarea1_inf239.dbo.Carrito;
+delete from test_tarea1_inf239.dbo.Oferta;
+delete from test_tarea1_inf239.dbo.productos;
+
 select * from test_tarea1_inf239.dbo.Oferta;
 select * from test_tarea1_inf239.dbo.Boleta;
 select * from test_tarea1_inf239.dbo.Carrito;
 select * from test_tarea1_inf239.dbo.productos;
 
-delete from test_tarea1_inf239.dbo.Boleta;
-delete from test_tarea1_inf239.dbo.Carrito;
-delete from test_tarea1_inf239.dbo.Oferta;
+drop table test_tarea1_inf239.dbo.Boleta;
+drop table test_tarea1_inf239.dbo.Carrito;
+drop table test_tarea1_inf239.dbo.Oferta;
+drop table test_tarea1_inf239.dbo.productos;
+
+/* PRODUCTO REPETIDO AL ELIMINAR */
+
+select * from test_tarea1_inf239.dbo.productos where prod_name = 'Agua Cristal';
+
+select prod_name,count(*) from test_tarea1_inf239.dbo.productos group by prod_name;
